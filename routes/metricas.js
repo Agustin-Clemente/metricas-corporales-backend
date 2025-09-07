bf7743dd6ce7c454f8128fe92d4924e856492d36
Agush <a_clemente@live.com.ar>
2025-09-07 01:30:32 -0300

    put y delete

--- routes/metricas.js
+++ routes/metricas.js
@@ -33,4 +33,28 @@ router.post('/', async (req, res) => {
     }
  });
 
+ router.put('/:id', async (req, res) => {
+    try {
+        const metricaActualizada = await Metrica.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!metricaActualizada) {
+            return res.status(404).json({ message: 'Métrica no encontrada' });
+        }
+        res.json(metricaActualizada);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}); 
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const metricaEliminada = await Metrica.findByIdAndDelete(req.params.id);
+        if (!metricaEliminada) {
+            return res.status(404).json({ message: 'Métrica no encontrada' });
+        }
+        res.json({ message: 'Métrica eliminada con éxito' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 export default router;
